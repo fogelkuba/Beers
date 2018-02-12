d8ed977f31700d2b79f21d004ce3d536634e36b0
fogelkuba <fogelkuba@gmail.com>
2018-02-13 00:04:57 +0100

    styling

--- src/components/Beer.vue
+++ src/components/Beer.vue
@@ -2,8 +2,8 @@
     <div class="col-md-4 col-sm-6">
         <div class="item-card">
             <div class="card-block">
-                <img class="item-image" :src="item.image_url" :alt="item.name">
                 <h3 class="card-title">{{item.name}}</h3>
+                <img class="item-image" :src="item.image_url" :alt="item.name">
                 <hr>
                 <p class="item-describtion">"{{item.tagline}}"</p>
                 <!-- <p class="item-describtion">{{item.description}}</p> -->
