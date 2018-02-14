9d654f37ad93a4ab6d1f156326d0610c0b70c3e7
fogelkuba <fogelkuba@gmail.com>
2018-02-14 23:56:04 +0100

    style h1 color uppercase

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
         <div class="container">
-            <h1>Beers <span>App</span></h1>
+            <h1>Beers<span>App</span></h1>
             <router-view>
             </router-view>
         </div>
