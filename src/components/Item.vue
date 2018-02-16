95e2eed0c3e9685aaf9dba0e7e12d3edf596c58e
fogelkuba <fogelkuba@gmail.com>
2018-02-16 22:32:47 +0100

    store and item desription working

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,7 +1,6 @@
 <template>
-        <div class="col-md-4 col-sm-6 col-xs-12 col-lg-3">
-            <!-- <div class="item-card" @click="itemRouter()"> -->
-            <router-link :to="{ path: `/discover/details`}">
+        <div class="col-md-4 col-sm-6 col-xs-12 col-lg-3" @click="passItem()">
+            <router-link :to="{ path: `/discover/details`}" >
                 <div class="item-card">
                     <div class="card-block">
                         <div class="card-title-container">
@@ -22,6 +21,13 @@
         data(){
             return {
                 item: {}
+            }
+        },
+        methods: {
+            passItem(){
+                this.$store.state.item = this.passedItem;
+                console.log(this.item);
+                console.log(this.passedItem)
             }
         },
         created(){
--- src/components/ItemDetails.vue
+++ src/components/ItemDetails.vue
@@ -1,7 +1,32 @@
 <template>
     <div class="">
         <div class="container">
-            <h2>Item Details</h2>
+            <div class="row">
+                <div class="col-md-3">
+                    <img :src="item.image_url" alt="">
+                </div>
+                <div class="col-md-9">
+                    <h2>{{item.name}}</h2>
+                    <h4>{{item.tagline}}</h4>
+                    <p>Since: {{item.first_brewed}}</p>
+                    <span><strong>IBU:</strong>{{item.ibu}}</span>
+                    <span><strong>ABV:</strong>{{item.abv}}%</span>
+                    <span><strong>EBC:</strong>{{item.ebc}}</span>
+                    <p>{{item.description}}</p>
+
+                    <h4>Best with:</h4>
+                    <ul>
+                        <li v-for="food in item.food_pairing">
+                            {{food}}
+                        </li>
+                    </ul>
+
+                    <h4>Method:</h4>
+                    <ul>{{item.method}}</ul>
+
+
+                </div>
+            </div>
         </div>
         <div class="col-md-12">
             <div class="row">
@@ -10,3 +35,16 @@
         </div>
     </div>
 </template>
+
+<script>
+    export default {
+        data(){
+            return {
+                item: {title: 'title'}
+            }
+        },
+        created(){
+            this.item = this.$store.state.item;
+        }
+    }
+</script>
